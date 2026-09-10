f33cd5a229b57fb2817da300d58e42b59c884b3b
Maro200611 <marwankhaliad16@gmail.com>
2026-09-10 12:12:12 +0300

    Update App.py

--- final tast project/App.py
+++ final tast project/App.py
@@ -45,7 +45,7 @@ from PIL import Image, ImageDraw, ImageFont
 # ======================================================================
 # CONFIG — edit paths to match your machine
 # ======================================================================
-DATA_DIR = r"D:\Downloads\final tast project" # <— change if needed
+DATA_DIR = os.path.join(BASE_DIR, "final tast project")# <— change if needed
 
 MODEL_PATH             = os.path.join(DATA_DIR, "best_xgboost_fit_model.pkl")
 SCALER_PATH            = os.path.join(DATA_DIR, "numeric_scaler.pkl")
